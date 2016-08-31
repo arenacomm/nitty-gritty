4b719b8d9bb622d805431919207134f6ba2f754d
Dustin Taylor <dustingtaylor@gmail.com>
2016-08-30 23:54:48 -0600

    added child theme footer for use with ACF and removed unnecessary uploads files

---
+++ wp-content/themes/aobase-child/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package AO_Base
+ */
+
+?>
+	</div><!-- #content -->
+
+<?php /*
+	<footer id="colophon" class="site-footer padder_sm" role="contentinfo">
+		<div class="container">
+			<?php the_field('footer_text', 'option'); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+*/ ?>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
